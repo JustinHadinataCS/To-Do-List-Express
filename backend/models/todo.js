38c042b4abda31e719930c6dde372952437435f5
Justin Hadinata <justinhadinata.23@gmail.com>
2025-05-06 17:52:52 +0700

    Add auth.js on the middleware

--- backend/models/todo.js
+++ backend/models/todo.js
@@ -1,25 +1,33 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Todo.belongsTo(models.User, { foreignKey: "UserId" });
     }
   }
-  Todo.init({
-    title: DataTypes.STRING,
-    completed: DataTypes.BOOLEAN,
-    UserId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Todo',
-  });
+
+  Todo.init(
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      completed: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      UserId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Todo",
+    }
+  );
+
   return Todo;
 };
